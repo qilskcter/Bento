7601c2439b08936b5aded8a30c5c5113d48c07e4
Khanh Nguyen <96412559+qilskcter@users.noreply.github.com>
2022-02-23 14:05:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherKey: 'ffc64f46165aa2a5cd91269cc33dc6f7', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
